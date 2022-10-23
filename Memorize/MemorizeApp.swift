1157aa0531bdc597a62c9bc4e1ed9caadfa55fb1
developer2locarmais <leonardosfonseca@icloud.com>
2022-10-23 15:29:47 -0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Leonardo Fonseca on 23/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
